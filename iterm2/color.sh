6be5051dd5151e41b7917093c77e27cdd26fcedc
Daichi HIRATA <bunny.hop.md@gmail.com>
2015-10-14 18:02:49 +0900

    remove unused config

--- iterm2/color.sh
+++ iterm2/color.sh
@@ -1,3 +1,3 @@
 ghq get https://github.com/mbadolato/iTerm2-Color-Schemes.git
 
-# open src/github.com/mbadolato/iTerm2-Color-Schemes/schemes/Japanesque.itermcolors
+open "$(ghq root)/github.com/mbadolato/iTerm2-Color-Schemes/schemes/Japanesque.itermcolors"
